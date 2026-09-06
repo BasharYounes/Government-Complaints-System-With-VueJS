477e5d400eb715e56f04af7c64fe186345b59799
BasharYounes <bywns3777@gmail.com>
2026-09-07 00:40:38 +0300

    Force HTTPS URLs in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,15 +2,13 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use App\Models\Complaint;
 use App\Observer\ComplaintObserver;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         if (
@@ -23,11 +21,12 @@ class AppServiceProvider extends ServiceProvider
         }
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Complaint::observe(
             ComplaintObserver::class
         );
